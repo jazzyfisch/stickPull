0255f7c80805a3f6e49cf261e36759bc4570a283
jazzyfisch <fischlij@student.ethz.ch>
2021-03-05 09:08:50 +0100

    Create buttons.py
    Create file to test buttons

---
+++ test/buttons.py
@@ -0,0 +1,34 @@
+import RPi.GPIO as GPIO
+
+# PINS
+button_L2 = 13 # config.buttons["button_R1"] L2
+button_L1 = 6 # L1
+button_R1 = 11 # R1
+button_R2 = 5 #R2
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(button_R1, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+GPIO.setup(button_R2, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+GPIO.setup(button_L1, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+GPIO.setup(button_L2, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+GPIO.add_event_detect(button_R1, GPIO.FALLING, bouncetime=300)
+GPIO.add_event_detect(button_R2, GPIO.FALLING, bouncetime=300)
+GPIO.add_event_detect(button_L1, GPIO.FALLING, bouncetime=300)
+GPIO.add_event_detect(button_L2, GPIO.FALLING, bouncetime=300)
+    
+    
+while True:
+    '''
+    if not(GPIO.input(button_R1)):
+        print("button R1 is pressed")
+    if not(GPIO.input(button_L1)):
+        print("button L1 is pressed")
+        '''
+    if GPIO.event_detected(button_R1):
+        print("button R1")
+    if GPIO.event_detected(button_L1):
+        print("button L1")
+    if GPIO.event_detected(button_R2):
+        print("button R2")
+    if GPIO.event_detected(button_L2):
+        print("button L2")
